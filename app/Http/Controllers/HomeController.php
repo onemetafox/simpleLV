439896ff9f1cbf20d0726aee52421598ec7bc993
todaycodemaster <senirodev1994128@gmail.com>
2023-05-03 19:49:17 -0700

    finished simple landing page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function index(Request $request){
         $page_title = "Testing Vue";
         $page_content = "This is the testing of the inertia-laravel-vue3 project";
-
+        return Inertia::render('Landing', compact('page_title', 'page_content'));
     }
 }
--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -3,19 +3,23 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">{{page_title}}</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        {{page_content}}
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
+<script setup>
+    import { Link, Head } from '@inertiajs/vue3';
+    defineProps({ page_title: Object, page_content: Object });
+</script>
 <script>
     export default {
+        id:'landing',
         mounted() {
             console.log('Component mounted.')
         }
